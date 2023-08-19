4411f84fa3b3f911b09e2c9b49bb5445cd98ae6a
soyandrey20 <jhonyandreyburga@gmail.com>
2023-08-18 22:42:34 -0500

    update local host

---
+++ src/package1/Prueba.java
@@ -0,0 +1,11 @@
+package package1;
+
+/**
+ * Prueba
+ */
+public class Prueba {
+
+    public void test() {
+        System.out.println("cualquier string");
+    }
+}
---
+++ src/package2/Main.java
@@ -0,0 +1,10 @@
+package package2;
+
+import package1.Prueba;
+
+public class Main {
+    public static void main(String[] args) {
+        Prueba prueba = new Prueba();
+        prueba.test();
+    }
+}
